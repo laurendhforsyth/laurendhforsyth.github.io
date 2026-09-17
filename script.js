09d5089a9bd15275db792d6c5956ae6025e4769a
Lauren Hepburn <laurendawnhepburn@gmail.com>
2026-09-17 16:40:52 -0500

    Initial Commit
    Initial GitHub commit with current resources

---
+++ script.js
@@ -0,0 +1,9 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const navigation = document.querySelector('.nav');
+
+if (menuToggle && navigation) {
+  menuToggle.addEventListener('click', () => {
+    const isOpen = navigation.classList.toggle('open');
+    menuToggle.setAttribute('aria-expanded', String(isOpen));
+  });
+}
